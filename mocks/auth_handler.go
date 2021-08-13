fad9807333df2a400fcc05e06022648603a74841
Ubuntu <ubuntu@ip-172-67-22-252.ap-southeast-1.compute.internal>
2021-08-13 06:58:19 +0000

    update compiled proto

--- mocks/auth_handler.go
+++ mocks/auth_handler.go
@@ -603,6 +603,20 @@ func (_m *AuthHandler) ReplaceCasbinPolicy(_a0 context.Context, _a1 *commonproto
 	return r0
 }
 
+// RequestSSOToken provides a mock function with given fields: _a0, _a1, _a2
+func (_m *AuthHandler) RequestSSOToken(_a0 context.Context, _a1 *commonproto.CoreRequest, _a2 *commonproto.AuthResponse) error {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, *commonproto.CoreRequest, *commonproto.AuthResponse) error); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // RequestToken provides a mock function with given fields: _a0, _a1, _a2
 func (_m *AuthHandler) RequestToken(_a0 context.Context, _a1 *commonproto.CoreRequest, _a2 *commonproto.AuthResponse) error {
 	ret := _m.Called(_a0, _a1, _a2)
